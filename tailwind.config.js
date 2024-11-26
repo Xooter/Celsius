150bab240c806889eb3d5b32ad719af2d6259a52
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-11-26 20:16:07 -0300

    Colors and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        light: "#FBFBFB",
+        dark: "#222",
+        accent: "#C96868",
+      },
       fontFamily: {
         avocado: ["Avocado", "sans-serif"],
         avocadobold: ["AvocadoBold", "sans-serif"],
