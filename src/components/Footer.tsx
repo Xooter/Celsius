d12d60467ec1e05d30ad7476517963552556ff3b
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-11-27 15:19:51 -0300

    cards in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,10 @@ import Mail01Icon from "./HugeIcons/mail-01-stroke-rounded";
 import TelephoneIcon from "./HugeIcons/telephone-stroke-rounded";
 import WhatsappIcon from "./HugeIcons/whatsapp-stroke-rounded";
 
+import master from "../assets/banks/mc_symbol.svg";
+import mercadopago from "../assets/banks/MP_RGB_HANDSHAKE_color-azul_hori-izq.svg";
+import visa from "../assets/banks/visa-svgrepo-com.svg";
+
 export const Footer = () => {
   return (
     <footer className="flex items-center justify-between py-5 px-8">
@@ -28,6 +32,11 @@ export const Footer = () => {
         <div className="flex gap-1">
           <Location01Icon />
           <h2>La Rioja 128 - Cordoba Capital</h2>
+        </div>
+        <div className="flex justify-evenly">
+          <img src={mercadopago} width={100} height={40} />
+          <img src={visa} width={40} height={40} />
+          <img src={master} width={40} height={40} />
         </div>
       </div>
     </footer>
