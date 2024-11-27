ea51f923d0c6d66cf26bb43d50d77fe4cb37182e
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-11-27 11:53:48 -0300

    className searchinput fix

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -8,10 +8,10 @@ export const SearchInput: React.FC<SearchInputProps> = ({
   ...props
 }) => {
   return (
-    <div className="flex relative">
+    <div className={`flex relative ${className} w-fit`}>
       <Search01Icon className="absolute ml-2 top-1/2 -translate-y-1/2" />
       <input
-        className={`bg-light-100 rounded-lg py-1 px-2 pl-10 outline-none ring-1 ring-dark caret-dark ${className}`}
+        className="bg-light-100 rounded-lg py-1 px-2 pl-10 outline-none ring-1 ring-dark caret-dark"
         {...props}
       />
     </div>
