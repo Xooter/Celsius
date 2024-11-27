5656d846de3221dccf9c838668d6d97eddc4a92c
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-11-27 15:09:03 -0300

    Click to consultar

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,16 +1,34 @@
 import { Item } from "../models/Item";
+import WhatsappIcon from "./HugeIcons/whatsapp-stroke-rounded";
 
 export const ItemCard = ({ item }: { item: Item }) => {
   const defaultImg =
     "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fblogger.googleusercontent.com%2Fimg%2Fb%2FR29vZ2xl%2FAVvXsEiGSE5R1u2oeM6EFz2minm_W4Lx4EfjVewWJBOPiS6VrDCXojwQmZkW2XyaOZY2Iwln5g_QJkSxAtpB88Ag-0QJKNbS6bwAQQ1VTOmRY_TTHc0SpJWc43qUTaHiJjR72-BqIqdjJGfvDWPc5Ro4FLmN5jKD-TMT-q-8x0xcOrIJrGb3jlqro2qxEVfH%2Fs442%2FTransistor%2520MOSFET.png&f=1&nofb=1&ipt=e0f3fc62eeb64de4b2af93652e18f603536eaf123359a8e493b81c127f50d4d8&ipo=images";
 
+  function onItemClick(
+    _: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ): void {
+    window.open(
+      `https://wa.me/543513780769?text=Hola!%20Estoy%20interesado%20en%20${item.name}`,
+      "_blank",
+    );
+  }
+
   return (
-    <div className="flex-col ring-0 ring-dark rounded-lg p-2 w-60 h-full ">
+    <div className="flex-col ring-0 ring-dark rounded-sm p-2 w-60 h-full ">
       <img
-        className="w-full max-h-40 object-contain mx-auto rounded-lg ring-2 ring-black mb-2  aspect-square"
+        className="w-full max-h-40 object-contain mx-auto rounded-lg ring-0 ring-black mb-2  aspect-square"
         src={item.img || defaultImg}
       />
-      <h1 className="text-center">{item.name}</h1>
+      <h1 className="text-center mb-2">{item.name}</h1>
+      <button
+        className="flex items-center justify-center w-full py-1 border-2 border-dark rounded-sm
+        gap-2 px-6  font-medium  text-white transition-all hover:shadow-[0px_3px_0px_black] hover:translate-y-[0px] shadow-none translate-y-[3px]"
+        onClick={onItemClick}
+      >
+        <WhatsappIcon />
+        <a>Consultar</a>
+      </button>
     </div>
   );
 };
